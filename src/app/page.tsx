e9f6f5c7e1972506ed58abe1257e3213590d8868
viewtifuljim <jamescolinwilliams@outlook.com>
2026-08-02 17:10:02 +0100

    cv download

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -230,8 +230,14 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
+      <Button
+          href="/files/JamesWilliams_CV_2024.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          variant="secondary"
+          className="group mt-6 w-full"
+        >
+          Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
